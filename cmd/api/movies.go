24b208364bc2bd56472814bf2eda0ec7f21d7336
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-13 23:20:45 +0100

    feat: endpoints and restful routing

---
+++ cmd/api/movies.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// createMovieHandler for the "POST /v1/movies" endpoint
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+// showMovieHandler for the "GET /v1/movies/:id" endpoint
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Otherwise, interpolate the movie ID in a placeholder response.
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+}
