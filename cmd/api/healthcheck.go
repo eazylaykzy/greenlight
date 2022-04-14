af6e95e4b5a5dcc89a1bb82eb321f066b30ff9cf
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-14 17:00:22 +0100

    feat: formatting and enveloping responses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,13 +7,15 @@ import (
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
