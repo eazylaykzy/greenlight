b19970c43757ac526ee4644e27fbb8e290d408b7
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-14 11:31:02 +0100

    feat: fixed format JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -8,7 +8,12 @@ import (
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	js := fmt.Sprintf(`{"status": "available", "environment": %q, "version": %q}`, app.config.env, version)
+
+	// Set the "Content-Type: application/json" header on the response. If you forget to
+	// this, Go will default to sending a "Content-Type: text/plain; charset=utf-8" header instead.
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON as the HTTP response body.
+	w.Write([]byte(js))
 }
