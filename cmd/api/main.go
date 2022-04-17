86a304a9affbf9cd67744ffcc9414217d0aac7f9
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-17 14:29:31 +0100

    feat: connecting to postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // Declare a string containing the application version number. Later we'll generate this automatically at build time
@@ -19,6 +23,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 // Define an application struct to hold the dependencies for our HTTP handlers, helpers, and middleware.
@@ -35,11 +42,25 @@ func main() {
 	// Read the value of the port and env command-line flags into the config struct. Default to using the
 	// port number 8080 and the environment "development" if no corresponding flags are provided.
 	flag.IntVar(&cfg.port, "port", 8080, "API server port")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
 
 	// Initialize a new logger which writes messages to the standard out stream, prefixed with the current date and time
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// Call the openDB helper function to create the connection pool, passing in the config struct.
+	// If this returns an error, we log it and exit the application immediately
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// Defer a call to db.Close so that the connection pool is closed before the main function exits
+	defer db.Close()
+
+	// Also log a message to say that the connection pool has been successfully established
+	logger.Printf("database connection pool established")
 
 	// Declare an instance of the application struct, containing the config struct and the logger.
 	app := &application{
@@ -59,6 +80,29 @@ func main() {
 
 	// Start the HTTP server.
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+// openDB function returns a sql.DB connection pool.
+func openDB(cfg config) (*sql.DB, error) {
+	// Use sql.Open to create an empty connection pool, using the DSN from the config struct
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a context with a 5-second timeout deadline.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Use PingContext to establish a new connection to the database, passing in the context we created above as a parameter
+	// If the connection couldn't be established successfully within the 5 seconds deadline, then this will return an error
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the sql.DB connection pool
+	return db, nil
 }
