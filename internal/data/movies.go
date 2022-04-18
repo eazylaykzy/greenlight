a423eeae9ac772bfc3333f1fc13f497eae4bc557
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-18 13:40:33 +0100

    feat: fetching movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"github.com/eazylaykzy/greenlight/internal/validator"
 	"github.com/lib/pq"
 	"time"
@@ -39,7 +40,45 @@ func (m MovieModel) Insert(movie *Movie) error {
 
 // Get method for fetching a specific record from the movies table
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	return nil, nil
+	// The PostgreSQL bigserial type that we're using for the movie ID starts auto-incrementing at 1 by default, so we
+	// know that no movies will have ID values less than that. To avoid making an unnecessary database call, we take a
+	// shortcut and return an ErrRecordNotFound error straight away
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	// Define the SQL query for retrieving the movie data
+	query := `SELECT id, created_at, title, year, runtime, genres, version FROM movies WHERE id = $1`
+
+	// Declare a Movie struct to hold the data returned by the query
+	var movie Movie
+
+	// Execute the query using the QueryRow method, passing in the provided id value as a placeholder parameter, and
+	// scan the response data into the fields of the Movie struct. Importantly, notice that we need to convert the scan
+	// target for the genres' column using the pq.Array adapter function
+	err := m.DB.QueryRow(query, id).Scan(
+		&movie.ID,
+		&movie.CreatedAt,
+		&movie.Title,
+		&movie.Year,
+		&movie.Runtime,
+		pq.Array(&movie.Genres),
+		&movie.Version,
+	)
+
+	// Handle any errors. If there was no matching movie found, Scan will return
+	// a sql.ErrNoRows error. We check for this and return our custom ErrRecordNotFound error instead
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	// Otherwise, return a pointer to the Movie struct
+	return &movie, nil
 }
 
 // Update method for updating a specific record in the movies table
