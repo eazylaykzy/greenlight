7b76a006a07b8cdf226e5ffc8f207f3c5cb4265b
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-21 21:37:17 +0100

    feat: Global Rate Limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/time/rate"
 	"net/http"
 )
 
@@ -24,6 +25,23 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 			}
 		}()
 
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	// Initialize a new rate limiter which allows an average of 2 requests per second,
+	// with a maximum of 4 requests in a single ‘burst’
+	limiter := rate.NewLimiter(2, 4)
+
+	// The function we are returning is a closure, which 'closes over' the limiter variable
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Call limiter.Allow to see if the request is permitted, and if it's not, then we
+		// call the rateLimitExceededResponse helper to return a 429 Too Many Requests response
+		if !limiter.Allow() {
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
