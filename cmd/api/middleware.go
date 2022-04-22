ca09fb93d095ab26529dc66cdc32164c776293b2
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-22 05:55:32 +0100

    feat: Configuring the Rate Limiters

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -66,29 +66,33 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ip, _, err := net.SplitHostPort(r.RemoteAddr)
-		if err != nil {
-			app.serverErrorResponse(w, r, err)
-			return
-		}
+		// Only carry out the check if rate limiting is enabled
+		if app.config.limiter.enabled {
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				app.serverErrorResponse(w, r, err)
+				return
+			}
 
-		mu.Lock()
+			mu.Lock()
 
-		if _, found := clients[ip]; !found {
-			// Create and add a new client struct to the map if it doesn't already exist
-			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
-		}
+			if _, found := clients[ip]; !found {
+				// Create and add a new client struct to the map if it doesn't already exist
+				clients[ip] = &client{limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst)}
+			}
 
-		// Update the last seen time for the client
-		clients[ip].lastSeen = time.Now()
+			// Update the last seen time for the client
+			clients[ip].lastSeen = time.Now()
 
-		if !clients[ip].limiter.Allow() {
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
+
 			mu.Unlock()
-			app.rateLimitExceededResponse(w, r)
-			return
 		}
 
-		mu.Unlock()
 		next.ServeHTTP(w, r)
 	})
 }
