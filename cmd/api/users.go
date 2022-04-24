1883a5e588d12f94afe27fee927e287c40c8fcd5
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-24 14:29:33 +0100

    feat: Sending Background Emails

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -62,16 +62,19 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	// Call Send() method on our Mailer, passing in the user's email address,
-	// name of the template file, and the User struct containing the new user's data
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+	// Launch a goroutine which runs an anonymous function that sends the welcome email.
+	go func() {
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		if err != nil {
+			// Importantly, if there is an error sending the email then we use the app.logger.PrintError
+			// helper to manage it, instead of the app.serverErrorResponse() helper like before.
+			app.logger.PrintError(err, nil)
+		}
+	}()
 
-	// Write a JSON response containing the user data along with a 201 Created status code
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	// Note that we also change this to send the client a 202 Accepted status code. This status code indicates
+	// that the request has been accepted for processing, but the processing has not been completed.
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
