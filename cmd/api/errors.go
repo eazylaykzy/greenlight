f89c26a717a2813fe95d9c632cc78fd60b928de7
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-25 06:40:07 +0100

    feat: Authenticating Requests

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -81,3 +81,11 @@ func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *htt
 	message := "invalid authentication credentials"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
+
+// invalidAuthenticationTokenResponse method will be used to send a 401 Unauthorized status code and JSON response to the client
+func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("WWW-Authenticate", "Bearer")
+
+	message := "invalid or missing authentication token"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
