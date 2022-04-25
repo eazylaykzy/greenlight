2db916d5a2c41699ed7ba9bc717d238644a4bfce
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-25 04:27:03 +0100

    feat: Activating a User

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -22,6 +22,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
 	// Return the httprouter instance.
 	return app.recoverPanic(app.rateLimit(router))
