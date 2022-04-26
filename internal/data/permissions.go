838c9758fa147b2ace007dea5de567cd29baccd3
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-26 15:53:29 +0100

    feat: Granting Permissions

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -3,6 +3,7 @@ package data
 import (
 	"context"
 	"database/sql"
+	"github.com/lib/pq"
 	"time"
 )
 
@@ -43,7 +44,9 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 		return nil, err
 	}
 
-	defer rows.Close()
+	defer func(rows *sql.Rows) {
+		_ = rows.Close()
+	}(rows)
 
 	var permissions Permissions
 
@@ -61,4 +64,17 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	}
 
 	return permissions, nil
+}
+
+// AddForUser add the provided permission codes for a specific user. Notice that we're using a
+// variadic parameter for the codes so that we can assign multiple permissions in a single call.
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `INSERT INTO users_permissions SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID, pq.Array(codes))
+
+	return err
 }
