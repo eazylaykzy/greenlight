d055edf474801959603300e8c17bc3479fa42e47
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-04-26 16:28:22 +0100

    feat: Simple CORS Requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -246,3 +246,30 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 	// Wrap this with the requireActivatedUser() middleware before returning it.
 	return app.requireActivatedUser(fn)
 }
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Add the "Vary: Origin" header.
+		w.Header().Add("Vary", "Origin")
+
+		// Get the value of the request's Origin header.
+		origin := r.Header.Get("Origin")
+
+		// Only run this if there's an Origin request header present.
+		if origin != "" {
+			// Loop through the list of trusted origins, checking to see if the request origin exactly matches
+			// one of them. If there are no trusted origins, then the loop won't be iterated.
+			for i := range app.config.cors.trustedOrigins {
+				if origin == app.config.cors.trustedOrigins[i] {
+					// If there is a match, then set an "Access-Control-Allow-Origin"
+					// response header with the request origin as the value and break out of the loop.
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+					break
+				}
+			}
+		}
+
+		// Call the next handler in the chain.
+		next.ServeHTTP(w, r)
+	})
+}
