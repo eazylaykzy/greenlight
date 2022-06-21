33477a91b5d11e37b60175aabc26a4706bff5572
Adeleke Adeniji <adelekeadenijio@gmail.com>
2022-06-22 00:02:16 +0100

    feat: Preflight CORS Requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -252,6 +252,9 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 		// Add the "Vary: Origin" header.
 		w.Header().Add("Vary", "Origin")
 
+		// Add the "Vary: Access-Control-Request-Method" header.
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
 		// Get the value of the request's Origin header.
 		origin := r.Header.Get("Origin")
 
@@ -264,6 +267,20 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 					// If there is a match, then set an "Access-Control-Allow-Origin"
 					// response header with the request origin as the value and break out of the loop.
 					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					// Check if the request has the HTTP method OPTIONS and contains the
+					// "Access-Control-Request-Method" header. If it does, then we treat it as a preflight request.
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						// Set the necessary preflight response headers.
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						// Write the headers along with a 200 OK status and return from
+						// the middleware with no further action.
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+
 					break
 				}
 			}
